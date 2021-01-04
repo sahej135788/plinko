727d1a9b5817fe416721b37b52ccc337f8ab5999
sahej135788 <71870197+sahej135788@users.noreply.github.com>
2021-01-04 16:51:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var plinko = []
+function setup() {
+  
+  createCanvas(500,500)
+  engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,460,1200,20);
+   division1 = new Division(50,400,20,100)
+   division2 = new Division(150,400,20,100)
+   division3 = new Division(250,400,20,100)
+   division4 = new Division(350,400,20,100)
+   division5 = new Division(450,400,20,100)
+   for(var i=0;i<500;i=i+80){
+   plinko.push(new Plinko(i,100))
+   }
+   for(var i=0;i<500;i=i+80){
+    plinko.push(new Plinko(i,200))
+    }
+    for(var i=0;i<500;i=i+80){
+      plinko.push(new Plinko(i,300))
+      }
+}
+
+function draw() {
+  background(255,255,255);  
+  Engine.update(engine);
+  division1.display()
+  division2.display()
+  division3.display()
+  division4.display()
+  division5.display()
+  ground.display()
+  for(var i=0;i<plinko.length;i++){
+  plinko[i].display()
+  }
+  drawSprites();
+}
